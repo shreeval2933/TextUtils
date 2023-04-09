7b4ff5534a2233874c68f7204f2d6044e757d255
shreeval2933 <shreevalpaladiya@gmail.com>
2023-04-09 20:33:06 +0530

    Updates

---
+++ precache-manifest.5326cd9ea1e329dbb213f4b3583096c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bce496f1af6db6015b11",
+    "url": "/TextUtils/static/css/main.b38edf8c.chunk.css"
+  },
+  {
+    "revision": "bce496f1af6db6015b11",
+    "url": "/TextUtils/static/js/main.bce496f1.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "eeac072fcc85ca05faa8",
+    "url": "/TextUtils/static/js/runtime~main.eeac072f.js"
+  },
+  {
+    "revision": "2b77c436685d0200f26721bfa1f6e0b9",
+    "url": "/TextUtils/index.html"
+  }
+];
